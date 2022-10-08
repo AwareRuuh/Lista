77bb7ffbc6dda66a0958d20c7c1b3ade279ddb37
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-08 17:42:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+     List<String> list=new ArrayList<String>();
+     list.add("Azul");  
+     list.add("Verde");  
+     list.add("Roxo");  
+    for(String lista:list)  
+  System.out.println(lista);  
+  }
+}
